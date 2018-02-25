620ced2871e1c73c19c336a74a5dffab831ab01d
magnuspl <magnuspeterlangeland@gmail.com>
2018-02-25 17:56:50 +0100

    init

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,19 +2,17 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
 import './index.css'
 
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Magnus Peter Langeland"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header />
     <div
       style={{
         margin: '0 auto',
