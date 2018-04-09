7ed7ead3456cc4b49e956771aaf66bf3bc831dab
magnuspl <magnuslangeland80@gmail.com>
2018-04-09 08:57:39 +0200

    added emojitab to projects

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,12 +12,13 @@ const IndexPage = () => (
         <p>I'm a computer science student from Oslo. I enjoy working with JavaScript and web technologies, but I know my way around other languages like Python and Java.</p>
         <p id="subtitle">Projects:</p>
         <p><a href="https://www.calculatortab.com/">Calculatortab</a>: Tiny calculator site. Built with ReactJS and GatsbyJS.</p>
+        <p><a href="https://www.emojitab.com/">Emojitab</a>: Emoji Search Engine. Built with jQuery</p>
         <p><a href="https://www.cryptolurker.com/">Cryptolurker</a>: Comprehensive Blockchain Glossary. Built with jQuery.</p>
         <p><a href="https://github.com/NTNUI/booking">NTNUi booking</a>: Booking application for NTNUi. Built with Python, Django and PostgreSQL.</p>
       </div>
       <div className="subtext">
         <p id="subtitle">Old projects:</p>
-        <p><a href="https://www.facebook.com/parrybot/">Parry</a>: Facebook bot made for NTNU students. The bot is no longer maintained and many of the built-in functions don't work anymore, but Parry will still talk back to you though. Built with Python, Flask and MySQL.</p>
+        <p><a href="https://www.facebook.com/parrybot/">Parry</a>: Facebook bot made for NTNU students. The bot is no longer maintained and many of the built-in functions does not work anymore, but you can still interact with the bot. Built with Python, Flask and MySQL.</p>
       </div>
       <div className="subtext">
         <p id="subtitle">Links:</p>
